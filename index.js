b1de0c22d7b628b4edfd467d6981fdf278251e0a
crabshank <44329154+crabshank@users.noreply.github.com>
2024-07-28 22:26:20 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ let mkps=document.getElementById('markPageSave');
 let svtx=document.getElementById('saveText');
 let txtar=document.getElementById('txta');
 let spt=document.getElementById('setupPatt');
+let frms=document.getElementById('frms');
+
+function sortByArrCols(arr, colsArr, dir){
+    arr.sort(sortFunction);
+		function sortFunction(a, b) {
+			for(let i = 0; i < arr.length; i++){
+					if(a[colsArr[i]]>b[colsArr[i]]){
+						return dir[i]*-1;
+					}else if(a[colsArr[i]]<b[colsArr[i]]){
+						return dir[i]*1;
+					}else if(i==arr.length-1){
+					return 0;
+				}
+			} 
+	}
+}
 
 window.onclick=(e)=>{
 	t=e.target;
@@ -41,18 +57,76 @@ function send(message) {
 		tabId: tid,
         title:tabs[0].title
       };
-      chrome.tabs.sendMessage(tid, msg);
+	  if(frms.childElementCount>0){
+		chrome.tabs.sendMessage(tid, msg,{frameId: parseInt(frms[frms.selectedIndex].getAttribute('fid'))});
+	  }else{
+		  chrome.tabs.sendMessage(tid, msg);
+	  }
     }
 
+}
+
+frms.oninput=(e)=>{
+	let p=frms[frms.selectedIndex];
+	let m=p.getAttribute('marked');
+	if(m=='1'){
+		mkp.style.display='none';
+		mkps.style.display='none';
+		txtar.style.display='none';
+		spt.style.display='';
+	}else{
+		mkp.style.display='';
+		mkps.style.display='';
+		txtar.style.display='';
+		spt.style.display='none';
+	}
 }
  
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     let m=message.message;
-	if(m==='marked'){
-		mkp.style.display='none';
-		mkps.style.display='none';
-		mkpsTxt.style.display='none';
-		txtar.style.display='none';
-		spt.style.display='';
+	let fch=[...frms.children];
+	let fids=sender.frameId.toString();
+	let fnd=false;
+	let toSort=[];
+	fch.forEach(f => {
+		let srt=[];
+		if(f.getAttribute('fid')==fids){
+			srt=[f,sender.frameId];
+			fnd=true;
+			f.setAttribute('loc', sender.url);
+			if(m==='marked'){
+				f.setAttribute('marked', 1);
+				srt.push(1);
+			}else{
+				f.setAttribute('marked', 0);
+				srt.push(0);
+			}
+			f.innerHTML=`Frame ${fids}: ${sender.url}`;
+		}else{
+			srt=[f,parseInt(f.getAttribute('fid')),parseInt(f.getAttribute('marked'))];
+		}
+		toSort.push(srt);
+	});
+	if(fnd===false){
+		let p=document.createElement('option');
+		frms.appendChild(p);
+		let srt=[p,sender.frameId];
+		p.setAttribute('fid', sender.frameId);
+		p.setAttribute('loc', sender.url);
+		if(m==='marked'){
+			p.setAttribute('marked', 1);
+			srt.push(1);
+		}else{
+			p.setAttribute('marked', 0);
+			srt.push(0);
+		}
+		p.innerHTML=`Frame ${fids}: ${sender.url}`;
+		toSort.push(srt);
 	}
+	sortByArrCols(toSort,[2,1],[1,-1]);
+	toSort.forEach(f =>{
+		frms.insertAdjacentElement('beforeend',f[0]);
+	});
+	frms.selectedIndex=0;
+	frms.dispatchEvent(new Event('input'));
 });
